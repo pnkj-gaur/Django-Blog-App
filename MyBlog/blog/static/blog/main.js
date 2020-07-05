10d5846633203e8a3ab580feec04bbc8a2909f2d
Pankaj <pankajgaur821@gmail.com>
2020-07-05 10:33:27 +0530

    added blog project

---
+++ MyBlog/blog/static/blog/main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidenav").style.height= "170px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.height = "0";
+    
+}
